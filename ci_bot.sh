4d815e9cf931b080364ec36da4ea80bc3ac751fb
Abhinav <122148266+hipexscape@users.noreply.github.com>
2024-06-15 18:20:35 +0530

    fixup! bot: echo `AUTHOR` in build-msgs

--- ci_bot.sh
+++ ci_bot.sh
@@ -141,7 +141,7 @@ if [ "$CONFIG_SYNC" == true ]; then
 
 <b>• ROM:</b> <code>$ROM_NAME</code>
 <b>• DEVICE:</b> <code>$DEVICE</code>
-<b>• AURHOR:</b> <code>$CONFIG_AUTHOR</code>
+<b>• AUTHOR:</b> <code>$CONFIG_AUTHOR</code>
 <b>• JOBS:</b> <code>$CONFIG_SYNC_JOBS Cores</code>
 <b>• DIRECTORY:</b> <code>$(pwd)</code>"
 
@@ -172,7 +172,7 @@ if [ "$CONFIG_SYNC" == true ]; then
 
 <b>• ROM:</b> <code>$ROM_NAME</code>
 <b>• DEVICE:</b> <code>$DEVICE</code>
-<b>• AURHOR:</b> <code>$CONFIG_AUTHOR</code>
+<b>• AUTHOR:</b> <code>$CONFIG_AUTHOR</code>
 <b>• JOBS:</b> <code>$CONFIG_SYNC_JOBS Cores</code>
 <b>• DIRECTORY:</b> <code>$(pwd)</code>
 <i>Syncing took $MINUTES minutes(s) and $SECONDS seconds(s)</i>"
@@ -198,7 +198,7 @@ build_start_message="****** | <i>Compiling ROM...</i>
 
 <b>• ROM:</b> <code>$ROM_NAME</code>
 <b>• DEVICE:</b> <code>$DEVICE</code>
-<b>• AURHOR:</b> <code>$CONFIG_AUTHOR</code>
+<b>• AUTHOR:</b> <code>$CONFIG_AUTHOR</code>
 <b>• JOBS:</b> <code>$CONFIG_COMPILE_JOBS Cores</code>
 <b>• TYPE:</b> <code>$([ -n "$OFFICIAL" ] && echo "Official" || echo "Unofficial")</code>
 <b>• VARIANT:</b> <code>$VARIANT</code>"
